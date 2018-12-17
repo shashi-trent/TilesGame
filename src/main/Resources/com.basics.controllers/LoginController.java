e1e8b8ea63d06fd7ea68065c769edef7ff4970da
Shashi Ranjan <45854858+shashi-trent@users.noreply.github.com>
2018-12-17 21:04:10 +0530

    Update LoginController.java

--- src/main/Resources/com.basics.controllers/LoginController.java
+++ src/main/Resources/com.basics.controllers/LoginController.java
@@ -1,5 +1,8 @@
 package com.basics.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 
 
 public class LoginController 
@@ -7,6 +10,10 @@ public class LoginController
     
     public ModelAndView login(HttpServletRequest req, HttpServletResponse res)
     {
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(User.class);
+        
+        User user = ctx.getBean("user");
+        user.setName(req.getParameter("name"));
     
     }
 

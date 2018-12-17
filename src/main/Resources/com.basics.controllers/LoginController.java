8610c82c2fbe9ceaf5af2c61d1f694d5d5c29c9f
Shashi Ranjan <45854858+shashi-trent@users.noreply.github.com>
2018-12-17 20:50:05 +0530

    controller created for user-login

---
+++ src/main/Resources/com.basics.controllers/LoginController.java
@@ -0,0 +1,13 @@
+package com.basics.controllers;
+
+
+
+public class LoginController 
+{
+    
+    public ModelAndView login(HttpServletRequest req, HttpServletResponse res)
+    {
+    
+    }
+
+}

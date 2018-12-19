0eab92bf2dbc5b8e20a59a908bd6329fec3cb304
Shashi Ranjan <45854858+shashi-trent@users.noreply.github.com>
2018-12-19 10:26:35 +0530

    Update LoginController.java

--- src/main/Resources/com.basics.controllers/LoginController.java
+++ src/main/Resources/com.basics.controllers/LoginController.java
@@ -10,10 +10,18 @@ public class LoginController
     
     public ModelAndView login(HttpServletRequest req, HttpServletResponse res)
     {
-        ApplicationContext ctx = new AnnotationConfigApplicationContext(User.class);
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(PojosConfig.class);
         
         User user = ctx.getBean("user");
         user.setName(req.getParameter("name"));
+        user.setEmail(req.getParameter("email"));
+        user.setkey(req.getParameter("key"));
+        
+        ModelAndView mv = new ModelAndView();
+        mv.addObeject(user);
+        mv.setViewName("welcome");
+        
+        return mv;
     
     }
 
